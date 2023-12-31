75128888899bb2b9060e4abd26318973d19ef452
HerrNamenlos123 <HerrNamenlos123@gmail.com>
2023-12-31 14:50:29 +0100

    Add function jump and change theme

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,10 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-j>"] = { "]]", "Jump to next function"},
+    ["<C-k>"] = { "[[", "Jump to previous function"},
+    ["<A-j>"] = { ":m +1<CR>", "Move lines down by one"},
+    ["<A-k>"] = { ":m -2<CR>", "Move lines up by one"},
   },
   v = {
     [">"] = { ">gv", "indent"},
