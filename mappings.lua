bbfb1170cc24cb7e8fdc383027556923cb1f941f
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-01-05 11:59:19 +0100

    center cursor

--- mappings.lua
+++ mappings.lua
@@ -4,10 +4,10 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-j>"] = { "]m", "Jump to next scope"},
-    ["<C-k>"] = { "[m", "Jump to previous scope" },
-    ["<leader>j"] = { "]]", "Jump to next function"},
-    ["<leader>k"] = { "[[", "Jump to previous function" },
+    ["<C-j>zz"] = { "]m", "Jump to next scope"},
+    ["<C-k>zz"] = { "[m", "Jump to previous scope" },
+    ["<leader>jzz"] = { "]]", "Jump to next function"},
+    ["<leader>kzz"] = { "[[", "Jump to previous function" },
     ["<A-j>"] = { ":m +1<CR>", "Move lines down by one" },
     ["<A-k>"] = { ":m -2<CR>", "Move lines up by one" },
     ["<leader>pf"] = { ":Telescope find_files<CR>", "Find in all files" },
