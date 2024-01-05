4b07eaf03492c371504e7469e106a188e3f7fc65
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-01-05 11:50:50 +0100

    Add mappings

--- mappings.lua
+++ mappings.lua
@@ -4,14 +4,19 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-j>"] = { "]]", "Jump to next function"},
-    ["<C-k>"] = { "[[", "Jump to previous function"},
-    ["<A-j>"] = { ":m +1<CR>", "Move lines down by one"},
-    ["<A-k>"] = { ":m -2<CR>", "Move lines up by one"},
+    ["<C-j>"] = { "[m", "Jump to next scope"},
+    ["<C-k>"] = { "]m", "Jump to previous scope" },
+    ["<leader>j"] = { "[[", "Jump to next function"},
+    ["<leader>k"] = { "]]", "Jump to previous function" },
+    ["<A-j>"] = { ":m +1<CR>", "Move lines down by one" },
+    ["<A-k>"] = { ":m -2<CR>", "Move lines up by one" },
     ["<leader>pf"] = { ":Telescope find_files<CR>", "Find in all files" },
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
+  },
+  x = {
+    ["<leader>p"] = { [["_dP]], "I think replace a line in visual select mode" },
   },
 }
 
