f9153911b8aa3a4bea1d3e2604c0c56a47843a00
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-01-09 11:13:37 +0100

    Format and add autofix and speedtyper

--- mappings.lua
+++ mappings.lua
@@ -8,11 +8,13 @@ M.general = {
     ["<C-k>"] = { "[mzz", "Jump to previous scope" },
     ["k"] = { "kzz", "Move one line up and keep cursor centered" },
     ["j"] = { "jzz", "Move one line down and keep cursor centered" },
+    ["<S-G>"] = { "<S-G>zz", "Move to end of file and keep cursor centered" },
     ["<leader>j"] = { "]]zz", "Jump to next function"},
     ["<leader>k"] = { "[[zz", "Jump to previous function" },
     ["<A-j>"] = { ":m +1<CR>", "Move lines down by one" },
     ["<A-k>"] = { ":m -2<CR>", "Move lines up by one" },
     ["<leader>pf"] = { ":Telescope find_files<CR>", "Find in all files" },
+    ["<leader>af"] = { function() vim.lsp.buf.code_action() end, "LSP Auto-Fix" },
   },
   v = {
     [">"] = { ">gv", "indent" },
