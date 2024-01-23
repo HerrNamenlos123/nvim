b01f3731c8d3fd63893f5e5fa805265d4a52fa0f
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-01-23 17:55:51 +0100

    settings

--- plugins.lua
+++ plugins.lua
@@ -55,7 +55,7 @@ local plugins = {
     },
     {
         "Exafunction/codeium.nvim",
-        lazy = false,
+        event = "InsertEnter",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "hrsh7th/nvim-cmp",
@@ -64,8 +64,44 @@ local plugins = {
             require("codeium").setup {}
             require("cmp").setup {
                 sources = {
-                    { name = "codeium" }
-                }
+                    { name = "codeium" },
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "nvim_lua" },
+                    { name = "path" },
+                },
+            }
+        end,
+    },
+    {
+        "NvChad/nvterm",
+        config = function()
+            require("nvterm").setup {
+                terminals = {
+                    shell = vim.o.shell,
+                    list = {},
+                    type_opts = {
+                        float = {
+                            relative = "editor",
+                            row = 0.05,
+                            col = 0.08,
+                            width = 0.8,
+                            height = 0.8,
+                            border = "single",
+                        },
+                        horizontal = { location = "rightbelow", split_ratio = 0.6 },
+                        vertical = { location = "rightbelow", split_ratio = 0.6 },
+                    },
+                },
+                behavior = {
+                    autoclose_on_quit = {
+                        enabled = false,
+                        confirm = true,
+                    },
+                    close_on_exit = true,
+                    auto_insert = true,
+                },
             }
         end,
     },
