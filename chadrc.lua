33a207404c3ea12b6570c16be1c4c1a889fda01b
HerrNamenlos123 <HerrNamenlos123@gmail.com>
2024-04-03 21:28:07 +0200

    Add mappings

--- chadrc.lua
+++ chadrc.lua
@@ -18,10 +18,10 @@ M.plugins = "custom.plugins"
 -- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.o.expandtab = true
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 vim.api.nvim_set_option_value("colorcolumn", "90", {})
 vim.api.nvim_exec([[
   highlight ColorColumn guibg=#555555
