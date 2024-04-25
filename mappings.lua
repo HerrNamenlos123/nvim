49c6e9285aeb5a16c6002bcc45d6af60bde15913
HerrNamenlos123 <HerrNamenlos123@gmail.com>
2024-04-25 20:49:29 +0200

    add terminal exit

--- mappings.lua
+++ mappings.lua
@@ -4,12 +4,12 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-j>"] = { "]mzz", "Jump to next scope"},
+    ["<C-j>"] = { "]mzz", "Jump to next scope" },
     ["<C-k>"] = { "[mzz", "Jump to previous scope" },
     ["k"] = { "kzz", "Move one line up and keep cursor centered" },
     ["j"] = { "jzz", "Move one line down and keep cursor centered" },
     ["<S-G>"] = { "<S-G>zz", "Move to end of file and keep cursor centered" },
-    ["<leader>j"] = { "]]zz", "Jump to next function"},
+    ["<leader>j"] = { "]]zz", "Jump to next function" },
     ["<leader>k"] = { "[[zz", "Jump to previous function" },
     ["<A-j>"] = { ":m +1<CR>", "Move lines down by one" },
     ["<A-k>"] = { ":m -2<CR>", "Move lines up by one" },
@@ -23,6 +23,9 @@ M.general = {
   },
   x = {
     ["<leader>p"] = { [["_dP]], "I think replace a line in visual select mode" },
+  },
+  t = {
+    ["<leader><ESC>"] = { "<C-\\><C-n> ", "Exit insert mode in Terminal" },
   },
 }
 
